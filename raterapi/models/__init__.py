bf109efe2649376dd2649810ef8eca92e34355d8
Jaron-Lane <jaronwohlwend@icloud.com>
2021-02-15 13:45:43 -0600

    Created Rating model

--- raterapi/models/__init__.py
+++ raterapi/models/__init__.py
@@ -2,3 +2,4 @@ from .player import Player
 from .game import Game
 from .category import Category
 from .gamecategory import GameCategory
+from .rating import Rating
---
+++ raterapi/models/rating.py
@@ -0,0 +1,6 @@
+import django.db from models
+
+class Rating(models.Model):
+    rating = models.CharField(max_length=50)
+    player = models.ForeignKey("Player", on_delete=models.CASCADE)
+    game = models.ForeignKey("Game", on_delete=models.CASCADE)
